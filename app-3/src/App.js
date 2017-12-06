86095436844d09385bddb7a17a91d602c12bf6d2
Melody <Melody@Melodys-MacBook-Pro.local>
2017-12-06 08:41:33 -0700

    start question 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      foods: ['pizza','oatmeal', 'ice cream']
+    }
+  }
+
+  filter(value){
+    
+  }
+
   render() {
+    let foodsToDisplay = this.state.foods.map( (element, index) => {
+      return (
+        <h2 key={ index }>{ element }</h2>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <input onChange={event => filter(event.target.value)}/>
+        <div>
+          {foodsToDisplay}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
